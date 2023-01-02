2585f1043bff184a3c8edde062de68c0847034a9
Vlad <vpashenko14@gmail.com>
2023-01-02 21:59:40 +0600

    chto3

--- SpaceBattle.Lib.Test/Test_Collision.cs
+++ SpaceBattle.Lib.Test/Test_Collision.cs
@@ -3,15 +3,17 @@ using Xunit;
 using System.Collections.Generic;
 using Moq;
 using Hwdtech;
-public class TestCollision {
+public class TestCollision
+{
 
-    public TestCollision() {
+    public TestCollision()
+    {
         new Hwdtech.Ioc.InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
-        
+
         IoC.Resolve<ICommand>("IoC.Register", "CollisionGetData", (object[] args) =>
         {
-            return new List<List<int>>() {new List<int>(){-2, 1, 0, 1}};
+            return new List<List<int>>() { new List<int>() { -2, 1, 0, 1 } };
         }).Execute();
         IoC.Resolve<ICommand>("IoC.Register", "CollisionGetDeltas", (object[] args) =>
         {
@@ -32,37 +34,40 @@ public class TestCollision {
     }
 
     [Fact]
-    public void CollisionCreateTreeTest() {
-        List<List<int>> list = new(){new List<int>(){2, 3}, new List<int>(){2, 4}};
-        Dictionary<int, object> b = (Dictionary<int, object>) new CollisionCreateTreeStrategy().Execute(list);
+    public void CollisionCreateTreeTest()
+    {
+        List<List<int>> list = new() { new List<int>() { 2, 3 }, new List<int>() { 2, 4 } };
+        Dictionary<int, object> b = (Dictionary<int, object>)new CollisionCreateTreeStrategy().Execute(list);
         Assert.Equal(b.Count, 1);
-        Assert.Equal(((Dictionary<int, object>) b[2]).ContainsKey(4), true);
+        Assert.Equal(((Dictionary<int, object>)b[2]).ContainsKey(4), true);
 
     }
 
     [Fact]
-    public void CollisionGetDeltasTest() {
+    public void CollisionGetDeltasTest()
+    {
         Mock<IUObject> UObject1 = new Mock<IUObject>();
         Mock<IUObject> UObject2 = new Mock<IUObject>();
-        UObject1.Setup(x => x.GetProperty("Position")).Returns(new Vector (3, 1));
-        UObject2.Setup(x => x.GetProperty("Position")).Returns(new Vector (1, 2));
-        UObject1.Setup(x => x.GetProperty("Velocity")).Returns(new Vector (1, 1));
-        UObject2.Setup(x => x.GetProperty("Velocity")).Returns(new Vector (1, 2));
-        List<int> a = (List<int>) new CollisionGetDeltasStrategy().Execute(UObject1.Object, UObject2.Object);
+        UObject1.Setup(x => x.GetProperty("Position")).Returns(new Vector(3, 1));
+        UObject2.Setup(x => x.GetProperty("Position")).Returns(new Vector(1, 2));
+        UObject1.Setup(x => x.GetProperty("Velocity")).Returns(new Vector(1, 1));
+        UObject2.Setup(x => x.GetProperty("Velocity")).Returns(new Vector(1, 2));
+        List<int> a = (List<int>)new CollisionGetDeltasStrategy().Execute(UObject1.Object, UObject2.Object);
         Assert.Equal(a[0], -2);
         Assert.Equal(a[1], 1);
         Assert.Equal(a[2], 0);
         Assert.Equal(a[3], 1);
     }
     [Fact]
-    public void CollisionCheckTestPositive() {
+    public void CollisionCheckTestPositive()
+    {
 
         Mock<IUObject> UObject1 = new Mock<IUObject>();
         Mock<IUObject> UObject2 = new Mock<IUObject>();
-        UObject1.Setup(x => x.GetProperty("Position")).Returns(new Vector (3, 1));
-        UObject2.Setup(x => x.GetProperty("Position")).Returns(new Vector (1, 2));
-        UObject1.Setup(x => x.GetProperty("Velocity")).Returns(new Vector (1, 1));
-        UObject2.Setup(x => x.GetProperty("Velocity")).Returns(new Vector (1, 2));
+        UObject1.Setup(x => x.GetProperty("Position")).Returns(new Vector(3, 1));
+        UObject2.Setup(x => x.GetProperty("Position")).Returns(new Vector(1, 2));
+        UObject1.Setup(x => x.GetProperty("Velocity")).Returns(new Vector(1, 1));
+        UObject2.Setup(x => x.GetProperty("Velocity")).Returns(new Vector(1, 2));
 
         bool collisionStatus = IoC.Resolve<bool>("CollisionCheck", UObject1.Object, UObject2.Object);
 
@@ -70,14 +75,15 @@ public class TestCollision {
 
     }
     [Fact]
-    public void CollisionCheckTestNegitive() {
+    public void CollisionCheckTestNegitive()
+    {
 
         Mock<IUObject> UObject1 = new Mock<IUObject>();
         Mock<IUObject> UObject2 = new Mock<IUObject>();
-        UObject1.Setup(x => x.GetProperty("Position")).Returns(new Vector (2, 1));
-        UObject2.Setup(x => x.GetProperty("Position")).Returns(new Vector (1, 2));
-        UObject1.Setup(x => x.GetProperty("Velocity")).Returns(new Vector (1, 1));
-        UObject2.Setup(x => x.GetProperty("Velocity")).Returns(new Vector (1, 2));
+        UObject1.Setup(x => x.GetProperty("Position")).Returns(new Vector(2, 1));
+        UObject2.Setup(x => x.GetProperty("Position")).Returns(new Vector(1, 2));
+        UObject1.Setup(x => x.GetProperty("Velocity")).Returns(new Vector(1, 1));
+        UObject2.Setup(x => x.GetProperty("Velocity")).Returns(new Vector(1, 2));
 
         bool collisionStatus = IoC.Resolve<bool>("CollisionCheck", UObject1.Object, UObject2.Object);
 
--- SpaceBattle.Lib/Collision.cs
+++ SpaceBattle.Lib/Collision.cs
@@ -2,9 +2,11 @@ namespace SpaceBattle.Lib;
 
 using Hwdtech;
 
-public class CollisionCreateTreeStrategy : IStrategy{   
-    public object Execute(params object[] args) {
-        List<List<int>> collisionVectors = (List<List<int>>) args[0];
+public class CollisionCreateTreeStrategy : IStrategy
+{
+    public object Execute(params object[] args)
+    {
+        List<List<int>> collisionVectors = (List<List<int>>)args[0];
         Dictionary<int, object> tree = new();
         collisionVectors.ForEach(x =>
         {
@@ -18,38 +20,47 @@ public class CollisionCreateTreeStrategy : IStrategy{
         return tree;
     }
 }
-public class CollisionGetDeltasStrategy : IStrategy {
+public class CollisionGetDeltasStrategy : IStrategy
+{
 
-    public object Execute(params object[] args) {
-        IUObject UObject1 = (IUObject) args[0];
-        IUObject UObject2 = (IUObject) args[1];
+    public object Execute(params object[] args)
+    {
+        IUObject UObject1 = (IUObject)args[0];
+        IUObject UObject2 = (IUObject)args[1];
         List<int> deltas = new();
 
         Vector position1 = (Vector)UObject1.GetProperty("Position");
         Vector position2 = (Vector)UObject2.GetProperty("Position");
-        for (int i = 0; i < position1.cords.Count(); i++) {
+        for (int i = 0; i < position1.cords.Count(); i++)
+        {
             deltas.Add(position2.cords[i] - position1.cords[i]);
         }
         Vector velocity1 = (Vector)UObject1.GetProperty("Velocity");
         Vector velocity2 = (Vector)UObject2.GetProperty("Velocity");
-        for (int i = 0; i < velocity1.cords.Count(); i++) {
+        for (int i = 0; i < velocity1.cords.Count(); i++)
+        {
             deltas.Add(velocity2.cords[i] - velocity1.cords[i]);
         }
         return deltas;
     }
 }
-public class CollisionTreeSolutionStrategy : IStrategy {
-    public object Execute(params object[] args) {
-        Dictionary<int, object> tree = (Dictionary<int, object>) args[0];
-        List<int> deltas = (List<int>) args[1];
+public class CollisionTreeSolutionStrategy : IStrategy
+{
+    public object Execute(params object[] args)
+    {
+        Dictionary<int, object> tree = (Dictionary<int, object>)args[0];
+        List<int> deltas = (List<int>)args[1];
 
         Dictionary<int, object> step = tree;
 
-        for (int i = 0; i < deltas.Count() - 1; i++) {
-            if (step.ContainsKey(deltas[i])) {
+        for (int i = 0; i < deltas.Count() - 1; i++)
+        {
+            if (step.ContainsKey(deltas[i]))
+            {
                 step = (Dictionary<int, object>)step[deltas[i]];
             }
-            else {
+            else
+            {
                 return false;
             }
         }
@@ -57,11 +68,13 @@ public class CollisionTreeSolutionStrategy : IStrategy {
     }
 }
 
-public class CollisionCheckStrategy : IStrategy {
-    
-    public object Execute(params object[] args) {
-        IUObject UObject1 = (IUObject) args[0];
-        IUObject UObject2 = (IUObject) args[1];
+public class CollisionCheckStrategy : IStrategy
+{
+
+    public object Execute(params object[] args)
+    {
+        IUObject UObject1 = (IUObject)args[0];
+        IUObject UObject2 = (IUObject)args[1];
 
         List<List<int>> treeData = IoC.Resolve<List<List<int>>>("CollisionGetData"); //получил строки
         List<int> deltas = IoC.Resolve<List<int>>("CollisionGetDeltas", UObject1, UObject2); //получил дельты
