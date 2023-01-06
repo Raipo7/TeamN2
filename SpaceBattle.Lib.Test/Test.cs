9a2eb0eb403595ea949890a1ac96ce720e18bb7f
Vlad <vpashenko14@gmail.com>
2023-01-06 15:37:15 +0600

    qweqesd

---
+++ SpaceBattle.Lib.Test/Test.cs
@@ -0,0 +1,44 @@
+namespace SpaceBattle.Lib.Test;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Moq;
+using Hwdtech;
+
+public class Testing {
+    public Testing() {
+        new Hwdtech.Ioc.InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        IoC.Resolve<ICommand>("IoC.Register", "Object.MoveAndRotate.Get.CommandsName", (object[] args) =>
+        {
+            return new List<string>() {"MoveCommand", "RotateCommand"};
+        }).Execute();
+
+        IoC.Resolve<ICommand>("IoC.Register", "Create.MacroCommand", (object[] args) =>
+        {
+            return new CreateMacroCommandStategy().Execute(args);
+        }).Execute();
+
+    }
+    [Fact]
+    public void Testing2() {
+        Mock<IUObject> UObject = new Mock<IUObject>();
+        UObject.Setup(x => x.GetProperty("Position")).Returns(new Vector(0, 0));
+        UObject.Setup(x => x.GetProperty("Velocity")).Returns(new Vector(1, 1));
+
+        Mock<SpaceBattle.Lib.ICommand> mockMoveCommand = new Mock<SpaceBattle.Lib.ICommand>();
+        mockMoveCommand.Setup(x => x.Execute()).Verifiable();
+
+        Mock<SpaceBattle.Lib.ICommand> mockRotateCommand = new Mock<SpaceBattle.Lib.ICommand>();
+        mockRotateCommand.Setup(x => x.Execute()).Verifiable();
+
+        UObject.Setup(x => x.GetProperty("MoveCommand")).Returns(mockMoveCommand.Object);
+        UObject.Setup(x => x.GetProperty("RotateCommand")).Returns(mockRotateCommand.Object);
+
+        IoC.Resolve<MacroCommand>("Create.MacroCommand", UObject.Object, "Object.MoveAndRotate").Execute();
+
+        mockMoveCommand.Verify();
+        mockRotateCommand.Verify();
+    }
+}
---
+++ SpaceBattle.Lib/MacroCommand.cs
@@ -0,0 +1,26 @@
+namespace SpaceBattle.Lib;
+
+using Hwdtech;
+public class MacroCommand : ICommand {
+    public List<ICommand> commands;
+    public MacroCommand(List<ICommand> commands) {
+        this.commands = commands;
+    }
+    public void Execute() {
+        commands.ForEach(x => x.Execute());
+    }
+}
+
+public class CreateMacroCommandStategy : IStrategy {
+
+    public object Execute(params object[] args) {
+        IUObject UObject = (IUObject)args[0];
+        //List<string> properties = (List<string>)args.Skip(1).ToList().Cast<string>();
+        string macroName = (string)args[1] + ".Get.CommandsName";
+        List<string> commandsName = IoC.Resolve<List<string>>(macroName);
+        List<ICommand> commands = new();
+        commandsName.ForEach(x => commands.Add((ICommand)UObject.GetProperty(x)));
+        
+        return new MacroCommand(commands);
+    }
+}
