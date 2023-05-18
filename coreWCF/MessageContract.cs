cf43fe7571fb20d52b2a56651ee9c505595ffb02
Vlad <vpashenko14@gmail.com>
2023-05-18 23:13:25 +0600

    Endpoint and fix StartMoveCommand

---
+++ coreWCF/MessageContract.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using CoreWCF.OpenApi.Attributes;
+
+using SpaceBattle.Lib;
+namespace WCF;
+
+[DataContract(Name = "MessageContract", Namespace = "http://spacebattle.com")]
+internal class MessageContract : IMessage
+{
+    [DataMember(Name = "type", Order = 1)]
+    [OpenApiProperty(Description = "Игровой тип комманды")]
+    public string type { get; set; }
+    [DataMember(Name = "game id", Order = 2)]
+    [OpenApiProperty(Description = "ID потока/игры")]
+    public string gameId { get; set; }
+    [DataMember(Name = "game item id", Order = 3)]
+    [OpenApiProperty(Description = "ID игрового объекта")]
+    public string gameItemId { get; set; }
+    [DataMember(Name = "properties", Order = 4)]
+    [OpenApiProperty(Description = "Дополнительные параметры")]
+    public JsonDictionary innerDict { get; set; } //для обработки внутреннего словаря в json
+    
+    public IDictionary<string, object> properties
+    {
+        get
+        {
+            return (IDictionary<string, object>)innerDict.dict;
+        }
+    }
+}
---
+++ coreWCF/WebApi.cs
@@ -0,0 +1,20 @@
+﻿using CoreWCF;
+using Hwdtech;
+
+namespace WCF;
+
+[ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+internal class WebApi : IWebApi
+{
+    public void GetMessage(MessageContract param)
+    {
+        try
+        {
+            IoC.Resolve<SpaceBattle.Lib.ICommand>("WCF.SendMessage", param).Execute();  //7.2 - обработка сообщений
+        }
+        catch (System.Exception err)
+        {
+            System.Console.WriteLine("\n" + err + "\n");
+        }
+    }
+}
