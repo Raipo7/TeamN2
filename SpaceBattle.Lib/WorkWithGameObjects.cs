5520ef55e5c99427c464136b079132590d5db5eb
Raipo7 <raipot228@gmail.com>
2023-05-29 00:28:37 +0600

    FirstThinks

---
+++ SpaceBattle.Lib/WorkWithGameObjects.cs
@@ -0,0 +1,19 @@
+namespace SpaceBattle.Lib;
+using Hwdtech;
+
+public class WorkWithGameObjects
+{   
+    Dictionary<string, object> objects;
+    public WorkWithGameObjects(Dictionary<string, object> objects)
+    {
+        this.objects = objects;
+    }
+    public object GameObjectGet(string gameItemId)
+    {
+        return objects[gameItemId];
+    }
+    public void GameObjectDelete(string gameItemId)
+    {
+        objects.Remove(gameItemId);
+    }
+}
---
+++ SpaceBattle.Lib/WorkWithGameQueue.cs
@@ -0,0 +1,19 @@
+namespace SpaceBattle.Lib;
+using Hwdtech;
+
+public class WorkWithGameQueue
+{
+    Queue<ICommand> commandQueue;
+    public WorkWithGameQueue(Queue<ICommand> commandQueue)
+    {
+        this.commandQueue = commandQueue;
+    }
+    public void GameQueuePush(ICommand command)
+    {
+        commandQueue.Enqueue(command);
+    }
+    public ICommand GameQueuePop()
+    {
+        return (ICommand) commandQueue.Dequeue();
+    }
+}
