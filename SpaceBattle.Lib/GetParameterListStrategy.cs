515716b289cd900cc6d11b50f20b33b2611b4d52
Raipo7 <raipot228@gmail.com>
2023-06-07 00:09:04 +0600

    удален комментарий

--- SpaceBattle.Lib/GetParameterListStrategy.cs
+++ SpaceBattle.Lib/GetParameterListStrategy.cs
@@ -8,16 +8,6 @@ public class GetParameterListStrategy : IStrategy
         MethodInfo method = (MethodInfo) args[0];
         ParameterInfo[] parameters = method.GetParameters();
         int key = (int) args[1];
-        // if (key == 0 && parameters.Length > 0)
-        // {
-        //     string parameterList = string.Join(", ", parameters.Select(p => $"{p.ParameterType} {p.Name}"));
-        //     return parameterList;
-        // }
-        // else if (key == 1 && parameters.Length > 0)
-        // {
-        //     string parameterList = ", " + string.Join(" ,", parameters.Select(p => $"{p.Name}"));
-        //     return parameterList;  
-        // }
         if (parameters.Length > 0)
         {
             if (key == 0)
